d3871ac9c59c70193d0ceceeec8678bf8a2b45eb
Gaurav Dhingra <gauravdhingra.gxyd@gmail.com>
2025-05-10 23:33:30 +0530

    changes for MRE

--- build_with_lfortran.sh
+++ build_with_lfortran.sh
@@ -1,7 +1,7 @@
 mkdir build
 cd build
 
-FC="/Users/gxyd/OpenSource/lfortran/src/bin/lfortran" cmake .. -DCMAKE_TOOLCHAIN_FILE=../toolchain-lfortran.cmake
+FC="/Users/gxyd/OpenSource/lfortran/src/bin/lfortran --no-warnings --no-style-warnings" cmake .. -DCMAKE_TOOLCHAIN_FILE=../toolchain-lfortran.cmake
 
 make tests VERBOSE=1
 
